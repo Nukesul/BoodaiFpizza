f78eb4fc4c8b9d8813b83e9b53d02d12b2b635c9
It developer 😎 <159410366+Nukesul@users.noreply.github.com>
2025-03-29 14:02:25 +0600

    215

--- backend/settings.py
+++ backend/settings.py
@@ -1,49 +1,24 @@
-"""
-Production-ориентированные настройки Django для проекта backend.
-"""
-
 from pathlib import Path
 import os
-import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Безопасность
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
-DEBUG = os.getenv('DJANGO_DEBUG', 'False') == 'True'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-%x&kg-nx75kirv^12#m@y0f486)4bphas#&6-m224qr%4iv$2o')
+
+DEBUG = os.getenv('DJANGO_DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = [
-    'boodaikg.com',
-    'www.boodaikg.com',
-    'vh438.timeweb.ru',
-    'nukesul-boodaifpizza-5206.twc1.net',
     'localhost',
     '127.0.0.1',
+    'vh438.timeweb.ru',
+    'boodaikg.com',
+    'www.boodaikg.com',
+    'nukesul-boodaifpizza-5206.twc1.net',  
     '0.0.0.0',
 ]
 
-# Порт сервера
-PORT = int(os.getenv('PORT', 3000))  # Установлен порт 3000 по умолчанию
+PORT = int(os.getenv('PORT', 8000))  # Вернул стандартный 8000 порт
 
-# Настройки базы данных (Production)
-DATABASES = {
-    'default': dj_database_url.config(
-        default='mysql://ch79145_boodai:16162007@vh438.timeweb.ru:3306/ch79145_boodai',
-        conn_max_age=600,
-        ssl_require=True
-    )
-}
-DATABASES['default']['OPTIONS'] = {
-    'charset': 'utf8mb4',
-    'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-}
-
-# Ускорение запросов
-DATABASES['default']['ENGINE'] = 'django.db.backends.mysql'
-DATABASES['default']['CONN_MAX_AGE'] = 300
-DATABASES['default']['ATOMIC_REQUESTS'] = True
-
-# Приложения
 INSTALLED_APPS = [
     'admin_interface',
     'colorfield',
@@ -59,10 +34,8 @@ INSTALLED_APPS = [
     'shop',
 ]
 
-# Middleware
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -72,31 +45,80 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-# Безопасность CORS/CSRF
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "https://boodaikg.com",
     "https://www.boodaikg.com",
     "https://nukesul-boodaifpizza-5206.twc1.net",
 ]
-CSRF_TRUSTED_ORIGINS = [
-    "http://localhost:3000",
-    "https://boodaikg.com",
-    "https://www.boodaikg.com",
-    "https://nukesul-boodaifpizza-5206.twc1.net",
-]
+
 CORS_ALLOW_CREDENTIALS = True
 
-# Оптимизация статики
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+CSRF_TRUSTED_ORIGINS = [
+    'http://localhost:3000',
+    'https://boodaikg.com',
+    'https://www.boodaikg.com',
+    'https://nukesul-boodaifpizza-5206.twc1.net',  
+]
 
-# Медиа файлы
+ROOT_URLCONF = 'backend.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / "shop" / "templates"],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'backend.wsgi.application'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ch79145_boodai',
+        'USER': 'ch79145_boodai',
+        'PASSWORD': '16162007',
+        'HOST': 'vh438.timeweb.ru',
+        'PORT': '3306',
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
+    }
+}
+
+AUTH_PASSWORD_VALIDATORS = [
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
+]
+
+LANGUAGE_CODE = 'ru-ru'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = False
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-# REST Framework
+FILE_UPLOAD_MAX_MEMORY_SIZE = 10 * 1024 * 1024
+DATA_UPLOAD_MAX_MEMORY_SIZE = 10 * 1024 * 1024
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
@@ -105,71 +127,44 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ],
-    'DEFAULT_RENDERER_CLASSES': [
-        'rest_framework.renderers.JSONRenderer',
-        'rest_framework.renderers.BrowsableAPIRenderer',
-    ],
-    'DEFAULT_THROTTLE_RATES': {
-        'anon': '100/hour',
-        'user': '1000/hour'
-    }
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 12,
 }
 
-# Безопасность в production
-if not DEBUG:
-    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-    SECURE_SSL_REDIRECT = True
-    SESSION_COOKIE_SECURE = True
-    CSRF_COOKIE_SECURE = True
-    SECURE_HSTS_SECONDS = 31536000
-    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-    SECURE_HSTS_PRELOAD = True
-    SECURE_CONTENT_TYPE_NOSNIFF = True
-    SECURE_BROWSER_XSS_FILTER = True
-    X_FRAME_OPTIONS = 'DENY'
-    SECURE_REFERRER_POLICY = 'same-origin'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-# Логирование
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
     'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-        },
+        'console': {'class': 'logging.StreamHandler'},
         'file': {
-            'level': 'DEBUG',
             'class': 'logging.FileHandler',
-            'filename': '/var/log/django/debug.log',
+            'filename': BASE_DIR / 'debug.log',
         },
     },
     'loggers': {
         'django': {
             'handlers': ['console', 'file'],
             'level': 'INFO',
-            'propagate': True,
+        },
+        'shop': {
+            'handlers': ['console', 'file'],
+            'level': 'DEBUG',
         },
     },
 }
 
-# Оптимизации
-TEMPLATES = [{
-    'BACKEND': 'django.template.backends.django.DjangoTemplates',
-    'DIRS': [BASE_DIR / "templates"],
-    'OPTIONS': {
-        'context_processors': [
-            'django.template.context_processors.debug',
-            'django.template.context_processors.request',
-            'django.contrib.auth.context_processors.auth',
-            'django.contrib.messages.context_processors.messages',
-        ],
-    },
-}]
-
-# Дополнительные настройки
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-LANGUAGE_CODE = 'ru-ru'
-TIME_ZONE = 'UTC'
-USE_I18N = True
-USE_L10N = True
-USE_TZ = False
+
+if not DEBUG:
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    SECURE_SSL_REDIRECT = True
+    SESSION_COOKIE_SECURE = True  
+    CSRF_COOKIE_SECURE = True    
+    SECURE_HSTS_SECONDS = 31536000
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_HSTS_PRELOAD = True
+    SECURE_CONTENT_TYPE_NOSNIFF = True
+    SECURE_BROWSER_XSS_FILTER = True
+    X_FRAME_OPTIONS = 'DENY'
